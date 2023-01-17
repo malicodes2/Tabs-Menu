eccc88d7b3657d23d4909abc4d7f9501cd7da2cf
Mohammad Ali <03340217mali@gmail.com>
2023-01-18 01:14:02 +0500

    js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,46 @@ function App() {
     setJobs(newJobs);
     setLoading(false);
   }
-  return <h2>tabs project setup</h2>
+
+  useEffect(() => {
+    fetchJobs();
+  }, [])
+
+  if (loading) {
+    return (
+      <section className='section loading'>
+        <h1>loading...</h1>
+      </section>
+    );
+  }
+
+  const { company, dates, duties, title } = jobs[value];
+
+  return (
+    <section className='section'>
+      <div className='title'>
+        <h2>Experience</h2>
+        <div className='underline'></div>
+      </div>
+      <div className='jobs-center'>
+        {/* btn container */}
+        {/* job info */}
+        <article className='job-info'>
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className='job-date'>{dates}</p>
+          {duties.map((duty, index)=>{
+            return (
+              <div key={index} className="job-desc">
+                <FaAngleDoubleRight className='job-icon' />
+                <p>{duty}</p>
+              </div>
+            )
+          })}
+        </article>
+      </div>
+    </section>
+  );
 }
 
 export default App
